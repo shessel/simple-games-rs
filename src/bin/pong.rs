e28962569cba6780ebc105fb6f54151394ba955f
shessel <shessel@users.noreply.github.com>
2019-08-06 22:55:56 +0200

    add bare-bones ball physics

--- src/bin/pong.rs
+++ src/bin/pong.rs
@@ -5,17 +5,41 @@ use ggez::{Context, GameResult};
 
 struct MainState {
     pos_x: f32,
+    pos_y: f32,
+    vel_x: f32,
+    vel_y: f32,
 }
 
 impl MainState {
     fn new() -> GameResult<MainState> {
-        Ok(MainState{ pos_x: 0.0})
+        Ok(MainState{ pos_x: 0.0, pos_y: 0.0, vel_x: 4.0, vel_y: 4.0})
     }
 }
 
 impl event::EventHandler for MainState {
     fn update(&mut self, _ctx: &mut Context) -> GameResult {
-        self.pos_x = (self.pos_x + 1.0) % 800.0;
+        self.pos_x = self.pos_x + self.vel_x;
+        self.vel_x = if self.pos_x > 800.0 && self.vel_x > 0.0 
+        {
+            -4.0
+        } else if self.pos_x < 0.0 && self.vel_x < 0.0 
+        {
+            4.0
+        } else
+        {
+            self.vel_x
+        };
+        self.pos_y = self.pos_y + self.vel_y;
+        self.vel_y = if self.pos_y > 600.0 && self.vel_y > 0.0 
+        {
+            -4.0
+        } else if self.pos_y < 0.0 && self.vel_y < 0.0 
+        {
+            4.0
+        } else
+        {
+            self.vel_y
+        };
         Ok(())
     }
 
@@ -24,7 +48,7 @@ impl event::EventHandler for MainState {
 
         let circle = graphics::Mesh::new_circle(ctx,
         graphics::DrawMode::fill(), nalgebra::Point2::new(0.0, 0.0), 100.0, 2.0, graphics::WHITE)?;
-        graphics::draw(ctx, &circle, (nalgebra::Point2::new(self.pos_x, 380.0),))?;
+        graphics::draw(ctx, &circle, (nalgebra::Point2::new(self.pos_x, self.pos_y),))?;
         graphics::present(ctx)?;
 
         Ok(())
