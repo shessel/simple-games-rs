8b35fc30d6ca4f208d7e0246ec1294d1a9b1731b
shessel <shessel@users.noreply.github.com>
2019-08-13 00:00:35 +0200

    add keyboard input

--- src/bin/pong.rs
+++ src/bin/pong.rs
@@ -1,5 +1,6 @@
-use ggez::event;
+use ggez::event::{self, EventHandler, KeyCode};
 use ggez::graphics;
+use ggez::input::keyboard;
 use ggez::nalgebra::{Point2, Vector2};
 use ggez::{Context, GameResult};
 
@@ -8,49 +9,80 @@ struct BallState {
     vel: Vector2<f32>,
 }
 
+#[derive(Clone, Copy)]
 struct PaddleState {
     pos: Point2<f32>,
 }
 
+#[repr(usize)]
+enum PaddlePosition {
+    Left,
+    Right,
+    Count,
+}
+
 struct MainState {
     ball: BallState,
-    paddles: [PaddleState; 2],
+    paddles: [PaddleState; PaddlePosition::Count as usize],
 }
 
 impl MainState {
     fn new() -> GameResult<MainState> {
         let ball = BallState {
             pos: Point2::new(0.0, 300.0),
-            vel: Vector2::new(4.0, 0.0),
+            vel: Vector2::new(4.0, 4.0),
         };
-        let left_paddle = PaddleState {
+
+        let mut paddles = [PaddleState {
+            pos: Point2::new(0.0, 0.0),
+        }; PaddlePosition::Count as usize];
+
+        paddles[PaddlePosition::Left as usize] = PaddleState {
             pos: Point2::new(10.0, 300.0),
         };
-        let right_paddle = PaddleState {
+
+        paddles[PaddlePosition::Right as usize] = PaddleState {
             pos: Point2::new(790.0, 300.0),
         };
-        let main_state = MainState {
-            ball,
-            paddles: [left_paddle, right_paddle],
-        };
+
+        let main_state = MainState { ball, paddles };
         Ok(main_state)
     }
 }
 
-impl event::EventHandler for MainState {
-    fn update(&mut self, _ctx: &mut Context) -> GameResult {
-        if self.ball.vel.x > 0.0 {
-            if (self.ball.pos.x - self.paddles[1].pos.x).abs() < 5.0
-                && (self.ball.pos.y - self.paddles[1].pos.y).abs() < 50.0
-            {
-                self.ball.vel.x *= -1.0;
+impl EventHandler for MainState {
+    fn update(&mut self, ctx: &mut Context) -> GameResult {
+        if keyboard::is_key_pressed(ctx, KeyCode::Up) {
+            for paddle in self.paddles.iter_mut() {
+                paddle.pos.y -= 4.0;
             }
-        } else if (self.ball.pos.x - self.paddles[0].pos.x).abs() < 5.0
-            && (self.ball.pos.y - self.paddles[0].pos.y).abs() < 50.0
-        {
-            self.ball.vel.x *= -1.0;
+        } else if keyboard::is_key_pressed(ctx, KeyCode::Down) {
+            for paddle in self.paddles.iter_mut() {
+                paddle.pos.y += 4.0;
+            }
         }
 
+        if self.ball.vel.x > 0.0 {
+            if (self.ball.pos.x - self.paddles[PaddlePosition::Right as usize].pos.x).abs() < 5.0
+                && (self.ball.pos.y - self.paddles[PaddlePosition::Right as usize].pos.y).abs()
+                    < 50.0
+            {
+                self.ball.vel.x *= -1.0;
+            } else if self.ball.pos.x > self.paddles[PaddlePosition::Right as usize].pos.x + 5.0 {
+                self.ball.vel.x *= -1.0;
+                self.ball.pos.x = 400.0;
+            }
+        } else {
+            if (self.ball.pos.x - self.paddles[PaddlePosition::Left as usize].pos.x).abs() < 5.0
+                && (self.ball.pos.y - self.paddles[PaddlePosition::Left as usize].pos.y).abs()
+                    < 50.0
+            {
+                self.ball.vel.x *= -1.0;
+            } else if self.ball.pos.x < self.paddles[PaddlePosition::Left as usize].pos.x - 5.0 {
+                self.ball.vel.x *= -1.0;
+                self.ball.pos.x = 400.0;
+            }
+        }
         if self.ball.pos.y < 0.0 && self.ball.vel.y < 0.0
             || self.ball.pos.y > 600.0 && self.ball.vel.y > 0.0
         {
