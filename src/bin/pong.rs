7329a20e776811bf96f09be98a3d5eef388a592e
shessel <shessel@users.noreply.github.com>
2019-08-13 23:32:17 +0200

    add game phases and player scores

--- src/bin/pong.rs
+++ src/bin/pong.rs
@@ -15,80 +15,120 @@ struct PaddleState {
 }
 
 #[repr(usize)]
-enum PaddlePosition {
+#[derive(PartialEq, Clone, Copy)]
+enum Player {
     Left,
     Right,
     Count,
 }
 
+#[derive(PartialEq)]
+enum Phase {
+    Paused,
+    Playing,
+    Serve(Player),
+}
+
 struct MainState {
     ball: BallState,
-    paddles: [PaddleState; PaddlePosition::Count as usize],
+    paddles: [PaddleState; Player::Count as usize],
+    score: [u8; Player::Count as usize],
+    phase: Phase,
 }
 
 impl MainState {
     fn new() -> GameResult<MainState> {
         let ball = BallState {
-            pos: Point2::new(0.0, 300.0),
+            pos: Point2::new(400.0, 300.0),
             vel: Vector2::new(4.0, 4.0),
         };
 
         let mut paddles = [PaddleState {
             pos: Point2::new(0.0, 0.0),
-        }; PaddlePosition::Count as usize];
+        }; Player::Count as usize];
 
-        paddles[PaddlePosition::Left as usize] = PaddleState {
+        paddles[Player::Left as usize] = PaddleState {
             pos: Point2::new(10.0, 300.0),
         };
 
-        paddles[PaddlePosition::Right as usize] = PaddleState {
+        paddles[Player::Right as usize] = PaddleState {
             pos: Point2::new(790.0, 300.0),
         };
 
-        let main_state = MainState { ball, paddles };
+        let score = [0u8; Player::Count as usize];
+
+        let main_state = MainState {
+            ball,
+            paddles,
+            score,
+            phase: Phase::Paused,
+        };
         Ok(main_state)
     }
 }
 
 impl EventHandler for MainState {
     fn update(&mut self, ctx: &mut Context) -> GameResult {
-        if keyboard::is_key_pressed(ctx, KeyCode::Up) {
-            for paddle in self.paddles.iter_mut() {
-                paddle.pos.y -= 4.0;
-            }
-        } else if keyboard::is_key_pressed(ctx, KeyCode::Down) {
-            for paddle in self.paddles.iter_mut() {
-                paddle.pos.y += 4.0;
-            }
-        }
-
-        if self.ball.vel.x > 0.0 {
-            if (self.ball.pos.x - self.paddles[PaddlePosition::Right as usize].pos.x).abs() < 5.0
-                && (self.ball.pos.y - self.paddles[PaddlePosition::Right as usize].pos.y).abs()
-                    < 50.0
-            {
-                self.ball.vel.x *= -1.0;
-            } else if self.ball.pos.x > self.paddles[PaddlePosition::Right as usize].pos.x + 5.0 {
-                self.ball.vel.x *= -1.0;
-                self.ball.pos.x = 400.0;
+        if self.phase == Phase::Paused {
+            if keyboard::is_key_pressed(ctx, KeyCode::Space) {
+                self.phase = Phase::Playing;
             }
         } else {
-            if (self.ball.pos.x - self.paddles[PaddlePosition::Left as usize].pos.x).abs() < 5.0
-                && (self.ball.pos.y - self.paddles[PaddlePosition::Left as usize].pos.y).abs()
-                    < 50.0
-            {
-                self.ball.vel.x *= -1.0;
-            } else if self.ball.pos.x < self.paddles[PaddlePosition::Left as usize].pos.x - 5.0 {
-                self.ball.vel.x *= -1.0;
-                self.ball.pos.x = 400.0;
+            if keyboard::is_key_pressed(ctx, KeyCode::Up) {
+                for paddle in self.paddles.iter_mut() {
+                    paddle.pos.y -= 4.0;
+                    paddle.pos.y = paddle.pos.y.max(60.0);
+                }
+            } else if keyboard::is_key_pressed(ctx, KeyCode::Down) {
+                for paddle in self.paddles.iter_mut() {
+                    paddle.pos.y += 4.0;
+                    paddle.pos.y = paddle.pos.y.min(540.0);
+                }
+            }
+
+            if let Phase::Serve(player) = &self.phase {
+                self.ball.pos.y = self.paddles[*player as usize].pos.y;
+                if keyboard::is_key_pressed(ctx, KeyCode::Space) {
+                    self.phase = Phase::Playing;
+                }
+            } else {
+                if self.ball.vel.x > 0.0 {
+                    if (self.ball.pos.x - self.paddles[Player::Right as usize].pos.x).abs() < 5.0
+                        && (self.ball.pos.y - self.paddles[Player::Right as usize].pos.y).abs()
+                            < 55.0
+                    {
+                        self.ball.vel.x *= -1.0;
+                    } else if self.ball.pos.x > self.paddles[Player::Right as usize].pos.x + 5.0 {
+                        self.ball.vel.x *= -1.0;
+                        self.ball.pos = self.paddles[Player::Right as usize].pos;
+                        self.ball.pos.x -= 10.0;
+                        self.score[Player::Left as usize] += 1;
+                        self.phase = Phase::Serve(Player::Right);
+                        println!("Score: {:?}", self.score);
+                    }
+                } else {
+                    if (self.ball.pos.x - self.paddles[Player::Left as usize].pos.x).abs() < 5.0
+                        && (self.ball.pos.y - self.paddles[Player::Left as usize].pos.y).abs()
+                            < 55.0
+                    {
+                        self.ball.vel.x *= -1.0;
+                    } else if self.ball.pos.x < self.paddles[Player::Left as usize].pos.x - 5.0 {
+                        self.ball.vel.x *= -1.0;
+                        self.ball.pos = self.paddles[Player::Left as usize].pos;
+                        self.ball.pos.x += 10.0;
+                        self.score[Player::Right as usize] += 1;
+                        self.phase = Phase::Serve(Player::Left);
+                        println!("Score: {:?}", self.score);
+                    }
+                }
+                if self.ball.pos.y < 0.0 && self.ball.vel.y < 0.0
+                    || self.ball.pos.y > 600.0 && self.ball.vel.y > 0.0
+                {
+                    self.ball.vel.y *= -1.0;
+                }
+                self.ball.pos += self.ball.vel;
             }
         }
-        if self.ball.pos.y < 0.0 && self.ball.vel.y < 0.0
-            || self.ball.pos.y > 600.0 && self.ball.vel.y > 0.0
-        {
-            self.ball.vel.y *= -1.0;
-        }
-        self.ball.pos += self.ball.vel;
         Ok(())
     }
 
